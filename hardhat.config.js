dc99a2cedb2c82c7e7517c9a40a5009cd5d8005a
zoro-onepiece <soraigds@gmail.com>
2026-03-07 14:58:50 +0500

    fixed hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,17 +1,21 @@
-require("@nomicfoundation/hardhat-ignition-ethers");
-require("dotenv").config();
+import "@nomicfoundation/hardhat-ignition";
+import "@nomicfoundation/hardhat-ignition-ethers";
+import "dotenv/config";
 
-module.exports = {
+export default {
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {
+      type: "edr-simulated", // Explicitly set the type for Hardhat's built-in network
       chainId: 31337,
     },
     localhost: {
+      type: "http", // Localhost connects via HTTP
       url: "http://127.0.0.1:8545",
       chainId: 31337,
     },
     sepolia: {
+      type: "http", // Sepolia is an external HTTP network
       url: process.env.SEPOLIA_RPC_URL || "https://sepolia.infura.io/v3/YOUR_INFURA_KEY",
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
       chainId: 11155111,
